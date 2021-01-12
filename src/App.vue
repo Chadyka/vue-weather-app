f514ebe7ebb296d7220766eaecc0539c008c5ff1
chadyka <adamjakab13@gmail.com>
2021-01-12 18:02:50 +0100

    init

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,23 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js App" />
+<div id="app">
+  <main>
+    <div class="search-box">
+      <input type="text" class="" placeholder="Search..."/>
+    </div>
+  </main>
+</div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
-
 export default {
   name: "App",
-  components: {
-    HelloWorld
+  data() {
+    return {
+      api_key: '60a0526a2377dd50692490338efb326c',
+    }
   }
 };
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
