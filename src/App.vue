5b810b883e0b0097df4311c30b50152740af9f2d
chadyka <adamjakab13@gmail.com>
2021-01-12 22:31:52 +0100

    divide into components

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,10 @@
 <template>
-  <div id="app" :class="typeof weather.main != 'undefined' && weather.main.temp > 16? 'warm':''">
+  <div
+    id="app"
+    :class="
+      typeof weather.main != 'undefined' && weather.main.temp > 16 ? 'warm' : ''
+    "
+  >
     <main>
       <div class="search-box">
         <input
@@ -11,22 +16,29 @@
         />
       </div>
       <div class="weather-wrap" v-if="typeof weather.main != 'undefined'">
-        <div class="location-box">
-          <div class="location">{{weather.name}}, {{weather.sys.country}}</div>
-          <div class="date">{{dateBuilder()}}</div>
-        </div>
-        <div class="weather-box">
-          <div class="temp">{{Math.round(weather.main.temp)}}°c</div>
-          <div class="weather">{{weather.weather[0].main}}</div>
-        </div>
+        <Location
+          :city="weather.name"
+          :country="weather.sys.country"
+          :date="dateBuilder()"
+        />
+        <Weather
+          :temperature="Math.round(weather.main.temp)"
+          :weather="weather.weather[0].main"
+        />
       </div>
     </main>
   </div>
 </template>
 
 <script>
+import Location from "./components/Location.vue";
+import Weather from "./components/Weather.vue";
 export default {
   name: "App",
+  components: {
+    Location,
+    Weather
+  },
   data() {
     return {
       api_key: "60a0526a2377dd50692490338efb326c",
@@ -37,19 +49,44 @@ export default {
   },
   methods: {
     fetchWeather(e) {
-      if (e.key=='Enter' && this.query != null) {
-        fetch(`${this.url_base}weather?q=${this.query}&units=metric&APPID=${this.api_key}`)
-          .then((res) => res.json())
-          .then((data) => {
+      if (e.key == "Enter" && this.query != null) {
+        fetch(
+          `${this.url_base}weather?q=${this.query}&units=metric&APPID=${this.api_key}`
+        )
+          .then(res => res.json())
+          .then(data => {
             this.weather = data;
-          })
+          });
       }
     },
-    dateBuilder () {
+    dateBuilder() {
       let date = new Date();
-      let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-      let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-      return `${days[date.getDay()]} ${date.getDate()} ${months[date.getMonth()]} ${date.getFullYear()}`;
+      let months = [
+        "January",
+        "February",
+        "March",
+        "April",
+        "May",
+        "June",
+        "July",
+        "August",
+        "September",
+        "October",
+        "November",
+        "December"
+      ];
+      let days = [
+        "Sunday",
+        "Monday",
+        "Tuesday",
+        "Wednesday",
+        "Thursday",
+        "Friday",
+        "Saturday"
+      ];
+      return `${days[date.getDay()]} ${date.getDate()} ${
+        months[date.getMonth()]
+      } ${date.getFullYear()}`;
     }
   }
 };
@@ -106,41 +143,5 @@ main {
   box-shadow: 0px 0px 16px rgba(0, 0, 0, 0.25);
   background-color: rgba(255, 255, 255, 0.75);
   border-radius: 16px 0px 16px 0px;
-}
-.location-box .location {
-  color: #fff;
-  font-size: 32px;
-  font-weight: 500;
-  text-align: center;
-  text-shadow: 1px 3px rgba(0, 0, 0, 0.25);
-}
-.location-box .date {
-  color: #fff;
-  font-size: 20px;
-  font-weight: 300;
-  font-style: italic;
-  text-align: center;
-}
-.weather-box {
-  text-align: center;
-}
-.weather-box .temp {
-  display: inline-block;
-  padding: 10px 25px;
-  color: #fff;
-  font-size: 102px;
-  font-weight: 900;
-  text-shadow: 3px 6px rgba(0, 0, 0, 0.25);
-  background-color: rgba(255, 255, 255, 0.25);
-  border-radius: 16px;
-  margin: 30px 0px;
-  box-shadow: 3px 6px rgba(0, 0, 0, 0.25);
-}
-.weather-box .weather {
-  color: #fff;
-  font-size: 48px;
-  font-weight: 700;
-  font-style: italic;
-  text-shadow: 3px 6px rgba(0, 0, 0, 0.25);
 }
 </style>
---
+++ src/components/Location.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="location-box">
+    <div class="location">{{ city }}, {{ country }}</div>
+    <div class="date">{{ date }}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    city: String,
+    country: String,
+    date: String
+  }
+};
+</script>
+
+<style lang="scss">
+.location-box .location {
+  color: #fff;
+  font-size: 32px;
+  font-weight: 500;
+  text-align: center;
+  text-shadow: 1px 3px rgba(0, 0, 0, 0.25);
+}
+.location-box .date {
+  color: #fff;
+  font-size: 20px;
+  font-weight: 300;
+  font-style: italic;
+  text-align: center;
+}
+</style>
